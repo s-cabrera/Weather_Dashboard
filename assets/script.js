d9d01fb89868eb3a2101a0f09e240e561845634f
Stephanie Cabrera <stephanie.cabrera.333@gmail.com>
2021-06-17 01:58:12 -0700

    Finsihed:five day forecast layout and functionality. Need: Store information in the localStorage, Get sidebar links to work

--- assets/script.js
+++ assets/script.js
@@ -11,6 +11,7 @@
 var sideBarEl = $('#sidebar');
 var myAlert = $('.alert');
 var dailyEl = $('#daily');
+var forecastEl = $('#five-days');
 var date = moment().format("dddd, MMMM Do YYYY");
 var dateUnix = moment().unix();
 
@@ -37,42 +38,75 @@ function uviColor(uvi){
 }
 
 function renderDailyForecast(city, temp, wind, humidity, uvi){
+    //Add border to the div
+    dailyEl.addClass('daily');
+    
     //Create elements
-    var cityHeader = $('<h2>');
-    var temp = $('<p>');
-    var wind = $('<p>');
-    var humidity = $('<p>');
-    var uvi = $('<p>');
-    var uviSpan = $('<span>');
+    var cityEl = $('<h2>');
+    var tempEl = $('<p>');
+    var windEl = $('<p>');
+    var humidityEl = $('<p>');
+    var uviEl = $('<p>');
+    var uviSpanEl = $('<span>');
 
     //Style them
-    cityHeader.text(`${city} (${date})`);
-    temp.text(`Temp: ${temp} °F`);
-    wind.text(`Wind: ${wind} MPH`);
-    humidity.text(`Humidity: ${humidity} %`);
-    uvi.text(`UV Index: `);
-    uviSpan.text(uvi);
-    uviSpan.css({'margin-left':'5px','color':'white', 'background': uviColor(uvi)})
+    cityEl.text(`${city} (${date})`);
+    tempEl.text(`Temp: ${temp} °F`);
+    windEl.text(`Wind: ${wind} MPH`);
+    humidityEl.text(`Humidity: ${humidity} %`);
+    uviEl.text(`UV Index: `);
+    uviSpanEl.text(uvi);
+    uviSpanEl.css({'text-shadow': '2px 2px black',
+    'padding':'4px','margin-left':'5px','color':'white', 'background': uviColor(uvi)})
 
     //Append them
-    dailyEl.append(cityHeader);
-    dailyEl.append(temp);
-    dailyEl.append(wind);
-    dailyEl.append(humidity);
-    dailyEl.append(uvi);
+    dailyEl.append(cityEl);
+    dailyEl.append(tempEl);
+    dailyEl.append(windEl);
+    dailyEl.append(humidityEl);
+    dailyEl.append(uviEl);
+    uviEl.append(uviSpanEl);
 
     //Put info into object
     var dailyObj = {
-        city: "city",
-        temp: "temp",
-        wind: "wind",
-        humidity: "humidity",
-        uvi: "uvi"   
+        city: city,
+        temp: temp,
+        wind: wind,
+        humidity: humidity,
+        uvi: uvi   
     };
 
 
     //Push object into array
     sidebarArray.push(dailyObj);
+}
+
+function renderFiveDayForecast(day, temp, wind, humidity, icon){
+    
+    //Create elements
+    var singleForecastEl = $('<div>');
+    var dayEl = $('<h4>');
+    var iconEl = $(`<image width="50px" height="50px" 
+    src="http://openweathermap.org/img/wn/${icon}@2x.png"/>`);
+    var tempEl = $('<p>');
+    var windEl = $('<p>');
+    var humidityEl = $('<p>');
+
+    //Style them
+    singleForecastEl.addClass('single_forecast');
+    dayEl.text(day);
+    tempEl.text(`Temp: ${temp} °F`);
+    windEl.text(`Wind: ${wind} MPH`);
+    humidityEl.text(`Humidity: ${humidity} %`);
+
+    //Append them
+    forecastEl.append(singleForecastEl);
+    singleForecastEl.append(dayEl);
+    singleForecastEl.append(iconEl);
+    singleForecastEl.append(tempEl);
+    singleForecastEl.append(windEl);
+    singleForecastEl.append(humidityEl);
+    
 }
 
 function callWeatherAPI(lat, lon, city){
@@ -82,19 +116,26 @@ function callWeatherAPI(lat, lon, city){
         return response.json();
     })
     .then(function (data) {
-    console.log('Fetch Response \n-------------');
-    console.log(data);
-    console.log(`${city} (${date})`);
-    console.log(`Temp: ${data.current.temp} °F`);
-    console.log(`Wind: ${data.current.wind_speed} MPH`);
-    console.log(`Humidity: ${data.current.humidity} %`);
-    console.log(`UV Index: ${data.current.uvi}`);
-    console.log(`Current weather icon code: ${data.current.weather[0].icon}`);
+        console.log('Fetch Response \n-------------');
+        console.log(data);
+        console.log(`${city} (${date})`);
+        console.log(`Temp: ${data.current.temp} °F`);
+        console.log(`Wind: ${data.current.wind_speed} MPH`);
+        console.log(`Humidity: ${data.current.humidity} %`);
+        console.log(`UV Index: ${data.current.uvi}`);
+        console.log(`Current weather icon code: ${data.current.weather[0].icon}`);
+        
+
+        renderSidebarItem(city, data.current.weather[0].icon);
+
+        renderDailyForecast(city, data.current.temp, data.current.wind_speed, 
+        data.current.humidity, data.current.uvi);
     
-
-    renderSidebarItem(city, data.current.weather[0].icon);
-    renderDailyForecast(city, data.current.temp, data.current.wind_speed, data.current.humidity, data.current.uvi);
-
+        for(let i = 0; i < 5; i++){
+            console.log(`Day: ${moment.unix(data.daily[i].dt).format("dddd")}`);
+            renderFiveDayForecast(moment.unix(data.daily[i].dt).format("dddd"), data.daily[i].temp.max, data.daily[i].wind_speed, 
+                data.daily[i].humidity, data.daily[i].weather[0].icon);
+        }
     });
 }
 
@@ -121,6 +162,9 @@ var searchBtnEventHandler = function(event){
         myAlert.css("display","block");
         return;
     }
+    dailyEl.removeClass('daily');
+    forecastEl.empty();
+    dailyEl.empty();
     callLongLatAPI($("input[aria-label='city']").val());
 }
 
